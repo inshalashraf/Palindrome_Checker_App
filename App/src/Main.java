6f56075407395718ca49ff662741bb2b881717a1
inshalashraf <mdinshalashraf@gmail.com>
2026-02-27 11:34:03 +0530

    Char Array Based

--- App/src/Main.java
+++ App/src/Main.java
@@ -2,22 +2,31 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+        String word = scn.nextLine();
 
-        System.out.println("Welcome to the Palindrome Checker Management System\n" +
-                "Version:\n" +
-                "1.0\n" +
-                "System initialized successfully.");
-        String original = scn.nextLine();
-        String reversed = "";
-        for (int i = original.length() - 1; i >= 0; i--) {
-            reversed = reversed + original.charAt(i);
+        char[] characters = word.toCharArray();
+
+
+        int start = 0;
+        int end = characters.length - 1;
+
+        boolean isPalindrome = true;
+
+        while(start < end) {
+            if(characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
 
-        if (original.equals(reversed)) {
-            System.out.println(original + " is a palindrome.");
+        if(isPalindrome) {
+            System.out.println(word + " is a palindrome.");
         } else {
-            System.out.println(original + " is not a palindrome.");
+            System.out.println(word + " is not a palindrome.");
         }
+
 
     }
 }
