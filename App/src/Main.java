48ac583da7fd4eb0787a1b323318fa2a5d266631
inshalashraf <mdinshalashraf@gmail.com>
2026-02-27 11:17:34 +0530

    Welcome Message

---
+++ App/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Welcome to the Palindrome Checker Management System\n" +
+                "Version:\n" +
+                "1.0\n" +
+                "System initialized successfully.");
+
+    }
+
+}
