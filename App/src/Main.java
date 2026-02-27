419303ca470bbdcfe38c19f942a11fb7162b9b00
inshalashraf <mdinshalashraf@gmail.com>
2026-02-27 11:25:31 +0530

    HardCoded

--- App/src/Main.java
+++ App/src/Main.java
@@ -1,13 +1,22 @@
 import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
-
+        Scanner scn = new Scanner(System.in);
 
         System.out.println("Welcome to the Palindrome Checker Management System\n" +
                 "Version:\n" +
                 "1.0\n" +
                 "System initialized successfully.");
-
+        String input = scn.nextLine();
+        boolean isPalindrome = true;
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
     }
 
 }
